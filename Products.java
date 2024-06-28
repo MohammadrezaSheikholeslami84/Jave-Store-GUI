a36b1fe0063253c739a73df7938302fc8861153a
iliyaasadi <153519525+iliyaasadi@users.noreply.github.com>
2024-06-28 03:45:02 +0330

    Update Products.java
    I have to commit it again because my team mate mohammadreza had problem to commit his code and it was because of my package format

--- Products.java
+++ Products.java
@@ -1,3 +1,5 @@
+package sign;
+
 import java.lang.classfile.attribute.StackMapTableAttribute;
 import java.sql.*;
 import java.util.ArrayList;
@@ -5,19 +7,21 @@ import java.util.ArrayList;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Products{
+    private int ID ;
     private String category ;
     private String name  ;
-    private int ID ;
-    private double price ;
+    private long price ;
     private int inventory ;
-    private double rating ;
-    public Products(String category , String name , int ID , double price , int inventory , double rating){
+    private int rating ;
+    private String filePath ;
+    public Products( int ID , String category , String name  , long price , int inventory , int rating , String filePath){
+        this.ID = ID ;
         this.category = category ;
         this.name = name ;
-        this.ID = ID ;
         this.price = price ;
         this.inventory = inventory ;
         this.rating = rating ;
+        this.filePath = filePath ;
     }
 
     public String getCategory() {
@@ -44,7 +48,7 @@ public class Products{
         this.ID = ID;
     }
 
-    public double getPrice() {
+    public long getPrice() {
         return price;
     }
 
@@ -60,52 +64,30 @@ public class Products{
         this.inventory = inventory;
     }
 
-    public double getRating() {
+    public int getRating() {
         return rating;
     }
 
-    public void setRating(double rating) {
+    public void setRating(int rating) {
         this.rating = rating;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
     }
 
     @Override
     public String toString() {
-        return this.name ;
-    }
-
-    public static void main(String[] args) {
-
-        try {
-            Products product1;
-            ArrayList<Products> list_of_products = new ArrayList<>() ;
-            String category = null;
-            String name = null ;
-            int id = -1  , inventory = -1 ;
-            double price = -1 , rating = -1 ;
-
-            Connection con = DriverManager.getConnection("jdbc:mysql://root@localhost:3306/sys", "root", "1284iliya");
-            Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,  ResultSet.CONCUR_UPDATABLE) ;
-            String SQL_query = "SELECT * FROM Products LIMIT 0,1" ;
-            ResultSet rs = stmt.executeQuery(SQL_query) ;
-            while(rs.next()) {
-                 category = rs.getString("category");
-                 name = rs.getString("name");
-                 id = rs.getInt("ID");
-                 price = rs.getDouble("price");
-                 inventory = rs.getInt("inventory");
-                 rating = rs.getDouble("rating");
-            }
-            product1 = new Products(category , name , id , price , inventory , rating) ;
-            list_of_products.add(product1) ;
-            System.out.println(product1.category + " " + product1.name + " " + product1.ID + " " + product1.price + " " + product1.inventory + " " + product1.rating);
-            System.out.println(list_of_products);
-
-
-        }
-        catch (SQLException e){
-            System.out.println(e.getMessage());
-        }
-
-
+        return "ID=" + ID +
+                ", category='" + category + '\'' +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", inventory=" + inventory +
+                ", rating=" + rating +
+                ", filePath='" + filePath + '\'' + '\n';
     }
 }
